009cb778fc1bc4746b7b0dd1564bbbf83a5bc765
Kishore <kishore07kzr@gmail.com>
2024-07-29 15:49:58 +0530

    Implemented Program to print the next string

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        System.out.println("Your num is: " + num);
+        String name= input.next();
+        System.out.println("Your num is: " + name);
     }
 }
