f1f972efcb2eab5c3b8d8e9ff433353c48ba59ed
Kishore <kishore07kzr@gmail.com>
2024-07-29 16:00:33 +0530

    implemented a simple calculator with addition and subtraction operations

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name= input.nextLine();
-        System.out.println("Your num is: " + name);
-    }
 
+        System.out.print("Enter int a");
+        int num1= input.nextInt();
+
+        System.out.print("Enter int b");
+        int num2= input.nextInt();
+
+        System.out.print("Input 1 for addition and any other num for subtraction");
+        int operation= input.nextInt();
+
+        int result;
+        if( operation == 1) {
+            result = num1 + num2;
+        } else {
+            if (num1 > num2) {
+                result = num1 - num2;
+            } else {
+                result = num2 - num1;
+            }
+        }
+
+        System.out.println("Result of your operation is: " + result);
+    }
 }
