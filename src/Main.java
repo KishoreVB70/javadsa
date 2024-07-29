36cdc08e82bfcfdbb6490bf70bcba1e6d49c59e9
Kishore <kishore07kzr@gmail.com>
2024-07-29 16:52:58 +0530

    implemented program to identify greatest of three inputs

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
 
-        System.out.print("Enter int a");
-        int num1= input.nextInt();
-
-        System.out.print("Enter int b");
-        int num2= input.nextInt();
-
-        System.out.print("Input 1 for addition and any other num for subtraction");
-        int operation= input.nextInt();
-
-        int result;
-        if( operation == 1) {
-            result = num1 + num2;
+        if (a >= b && a >= c) {
+            System.out.println("a is the largest");
+        } else if (b >= a && b >= c) {
+            System.out.println("b is the largest");
         } else {
-            if (num1 > num2) {
-                result = num1 - num2;
-            } else {
-                result = num2 - num1;
-            }
+            System.out.println("c is the largest");
         }
-
-        System.out.println("Result of your operation is: " + result);
     }
 }
