548787a4a6a19510660af2b9ac40a6bfbe849a10
Kishore <kishore07kzr@gmail.com>
2024-07-29 17:04:07 +0530

    added 2 separate functions into main, one to find the largest of three and one to find the case of a char

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        greatestOfThree();
+        upperOrLower();
+
+    }
+
+    private static void upperOrLower() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        char c = in.next().trim().charAt(0);
+        if (c > 90) {
+            System.out.println("Character is lower case");
+        } else {
+            System.out.println("character is upper case");
+        }
+    }
+
+    private static void greatestOfThree() {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
