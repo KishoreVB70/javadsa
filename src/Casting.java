8af5ddca2b01b32f8d8085b0a1e6871da3da30f1
Kishore <kishore07kzr@gmail.com>
2024-07-29 16:17:50 +0530

    example for explicit type casting with float back to int

--- src/Casting.java
+++ src/Casting.java
@@ -2,6 +2,8 @@ public class Casting {
     public static void main(String[] args) {
         float f2 = 22;
         System.out.println(f2);
+        int a = (int) f2;
+        System.out.println(a);
 
     }
 }
