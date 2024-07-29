f2084bf1138d14eb1157780f94131960b5fe518b
Kishore <kishore07kzr@gmail.com>
2024-07-29 16:28:27 +0530

    implemented an always false do while loop

--- src/Casting.java
+++ src/Casting.java
@@ -1,9 +1,9 @@
 public class Casting {
     public static void main(String[] args) {
         float f2 = 22;
-        System.out.println(f2);
-        int a = (int) f2;
-        System.out.println(a);
 
+        do{
+            System.out.println(f2);
+        } while (f2 < 2);
     }
 }
