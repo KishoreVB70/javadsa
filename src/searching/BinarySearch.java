3ac119b974c0ae51ea13dace0a8f0d04a6e5b8c2
Kishore <kishore07kzr@gmail.com>
2024-07-30 16:29:16 +0530

    implemeneted order agnostic binary search

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -3,15 +3,80 @@ package searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10,11,19,21};
+        int[] descArr = {17,12,11,10,9,8,7,6,5,4,3,2,1};
 
-        int target = 21;
+        int target = 6;
 
-        int result = simpleBinarySearch(arr, target);
+//        int result = simpleBinarySearch(arr, target);
+        int result = orderAgnostic(descArr, target);
         System.out.println(result);
+
+
+    }
+
+    static int orderAgnostic(int[] arr, int target) {
+        if (arr.length == 0 ) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+        while (end >= start) {
+            int i = (start + end) /2;
+
+            if (arr[i] == target) {
+                return i;
+            }
+
+            if (isAsc) {
+                if (target > arr[i]) {
+                    start = i + 1;
+                }
+                else {
+                    end = i - 1;
+                }
+            }
+            else {
+                if (target > arr[i]) {
+                    end = i - 1;
+                }
+                else {
+                    start = i + 1;
+                }
+            }
+        }
+        return -1;
 
     }
 
     static int simpleBinarySearch(int[] arr, int target) {
+        if (arr.length == 0 ) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+
+
+        while (end >= start) {
+            int i = (start + end) /2;
+
+            if (arr[i] == target) {
+                return i;
+            } else if (target > arr[i]) {
+                start = i + 1;
+            }else {
+                end = i - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    static int descBinarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
@@ -26,9 +91,9 @@ public class BinarySearch {
             if (arr[i] == target) {
                 return i;
             } else if (target > arr[i]) {
-                start = i + 1;
-            }else {
                 end = i - 1;
+            }else {
+                start = i + 1;
             }
         }
 
