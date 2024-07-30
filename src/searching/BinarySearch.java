e4d6a38dad2dec98d7793bd5b698eef19bd3ac60
Kishore <kishore07kzr@gmail.com>
2024-07-30 17:29:07 +0530

    Also implemented function to find the floor of a number

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -2,13 +2,13 @@ package searching;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,3,4,7,8,9,10,19,21};
+        int[] arr = {1,3,7,9,10,19,21};
         int[] descArr = {17,12,11,10,9,8,7,6,5,4,3,2,1};
 
-        int target = 7;
+        int target = 8;
 
 //        int result = simpleBinarySearch(arr, target);
-        int result = ceilingOfaNumber(arr, target);
+        int result = floorOfaNumber(arr, target);
         System.out.println(result);
 
 
@@ -54,6 +54,47 @@ public class BinarySearch {
         }
 
         return start;
+    }
+    static int floorOfaNumber(int[] arr, int target) {
+        if (arr.length == 0 ) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+        while (end >= start) {
+            int i = (start + end) / 2;
+
+            if (arr[i] == target) {
+                return i;
+
+            }
+
+            if (isAsc) {
+                if (target > arr[i]) {
+                    start = i + 1;
+                }
+                else {
+                    end = i - 1;
+                }
+            }
+            else {
+                if (target > arr[i]) {
+                    end = i - 1;
+                }
+                else {
+                    start = i + 1;
+                }
+            }
+        }
+
+        if (start > arr.length - 1) {
+            return  -1;
+        }
+
+        return end;
     }
 
     static int orderAgnostic(int[] arr, int target) {
