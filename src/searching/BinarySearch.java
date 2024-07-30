77644fe29e5f0fc37fd899170c8788147b0bd2f8
Kishore <kishore07kzr@gmail.com>
2024-07-30 17:20:09 +0530

    implemented function to find the ceiling of a number in an array using binary search

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -2,16 +2,58 @@ package searching;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,19,21};
+        int[] arr = {1,3,4,7,8,9,10,19,21};
         int[] descArr = {17,12,11,10,9,8,7,6,5,4,3,2,1};
 
-        int target = 6;
+        int target = 7;
 
 //        int result = simpleBinarySearch(arr, target);
-        int result = orderAgnostic(descArr, target);
+        int result = ceilingOfaNumber(arr, target);
         System.out.println(result);
 
 
+    }
+
+    static int ceilingOfaNumber(int[] arr, int target) {
+        if (arr.length == 0 ) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+
+        while (end >= start) {
+            int i = (start + end) / 2;
+
+            if (arr[i] == target) {
+                return i;
+
+            }
+
+            if (isAsc) {
+                if (target > arr[i]) {
+                    start = i + 1;
+                }
+                else {
+                    end = i - 1;
+                }
+            }
+            else {
+                if (target > arr[i]) {
+                    end = i - 1;
+                }
+                else {
+                    start = i + 1;
+                }
+            }
+        }
+
+        if (start > arr.length - 1) {
+            return  -1;
+        }
+
+        return start;
     }
 
     static int orderAgnostic(int[] arr, int target) {
