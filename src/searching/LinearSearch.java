7f35677cc64099d3c9bd5329dc88f65aec9406a3
Kishore <kishore07kzr@gmail.com>
2024-07-30 13:30:32 +0530

    Function to return the index of the found element and -1 for un found element

--- src/searching/LinearSearch.java
+++ src/searching/LinearSearch.java
@@ -3,18 +3,33 @@ package searching;
 public class LinearSearch {
     public static void main(String[] args) {
 
-        int[] bot = {1,2,3,4,6,7};
-        int t = 2;
-        boolean result = simpleLinearSearchInt(bot, t);
+        int[] arr = {1,2,3,4,6,7};
+        int target = 0;
+//        boolean result = simpleLinearSearchIntBoolean(bot, t);
+        int result = simpleLinearSearchIntIndex(arr, target);
         System.out.println(result);
     }
 
-    static boolean simpleLinearSearchInt(int[] arr, int target) {
+    static boolean simpleLinearSearchIntBoolean(int[] arr, int target) {
         for (int i: arr) {
             if (i == target) {
                 return true;
             }
         }
         return false;
+    }
+
+    static int simpleLinearSearchIntIndex(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
