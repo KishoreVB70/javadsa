872eec64700c97d9a7e5b3c00a0d4b0756f8a3df
Kishore <kishore07kzr@gmail.com>
2024-07-30 15:56:00 +0530

    Implemented basic binary search algo

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -2,6 +2,35 @@ package searching;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12};
 
+        int target = 10;
+
+        int result = simpleBinarySearch(arr, target);
+        System.out.println(result);
+
+    }
+
+    static int simpleBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length;
+
+        if (arr.length == 0 ) {
+            return -1;
+        }
+
+
+        while (end >= start) {
+            int i = (start + end) /2;
+            if (arr[i] == target) {
+                return i;
+            } else if (target > arr[i]) {
+                start = i + 1;
+            }else {
+                end = i - 1;
+            }
+        }
+
+        return -1;
     }
 }
