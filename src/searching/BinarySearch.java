19c0974f158918af90ba45abf0f8e0415993baed
Kishore <kishore07kzr@gmail.com>
2024-07-30 22:13:58 +0530

    final changes to the leet code problem of letter

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -2,18 +2,48 @@ package searching;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,3,7,9,10,19,21};
+        char[] arr = {'e', 'e','g', 'g'};
         int[] descArr = {17,12,11,10,9,8,7,6,5,4,3,2,1};
 
-        int target = 8;
+        char target = 'g';
 
 //        int result = simpleBinarySearch(arr, target);
-        int result = floorOfaNumber(arr, target);
+        char result = nextGreatestLetter(arr, target);
         System.out.println(result);
 
 
     }
 
+    static char nextGreatestLetter(char[] letters, char target) {
+        String url = "https://leetcode.com/problems/find-smallest-letter-greater-than-target/";
+        int start  = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int n = (start + end) / 2;
+            if (letters[n] == target) {
+                // If the target is the last element in the array
+                if(n == letters.length - 1) {
+                    return letters[0];
+                }
+                // return the greatest
+                start = n + 1 ;
+
+            }
+
+            if (target > letters[n]) {
+                start += 1;
+            } else if (target < letters[n]){
+                end -= 1;
+            }
+        }
+        if (start > (letters.length - 1)) {
+            return letters[0];
+        }
+
+        return letters[start];
+
+    }
     static int ceilingOfaNumber(int[] arr, int target) {
         if (arr.length == 0 ) {
             return -1;
@@ -96,7 +126,6 @@ public class BinarySearch {
 
         return end;
     }
-
     static int orderAgnostic(int[] arr, int target) {
         if (arr.length == 0 ) {
             return -1;
