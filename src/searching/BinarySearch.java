c17beff6c515e5e6ee2d6b54cb0c81b059e0fe54
Kishore <kishore07kzr@gmail.com>
2024-07-31 07:13:14 +0530

    Solved the first and last position problem using 2 binary search

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -6,17 +6,65 @@ public class BinarySearch {
     public static void main(String[] args) {
 //        char[] arr = {'e', 'e','g', 'g'};
         int[] descArr = {17,12,11,10,9,8,7,6,5,4,3,2,1};
-        int[] arr = {1,2,3,4,5,6,7,2,5,6,4,1};
+        int[] arr = {1,2,3,4,5,5,7,8,9,9};
 
         char target = 9;
 
 //        int result = simpleBinarySearch(arr, target);
 //        char result = nextGreatestLetter(arr, target);
-        int[] result = firstAndLastPositionLinearSearch(arr, target);
+        int[] result = firstAndLastPosition(arr, target);
         System.out.println(Arrays.toString(result));
 
 
 
+    }
+
+    static int[] firstAndLastPosition(int[] nums, int target) {
+        int[] position = {-1,-1};
+
+        int start = 0;
+        int end = nums.length - 1;
+
+
+        while(start <= end) {
+            int n = (start + end) / 2;
+            if (nums[n] == target) {
+                if (n > position[1]) {
+                    position[1] = n;
+                }
+                start += 1;
+            }
+
+            if (target > nums[n]) {
+                start += 1;
+            } else if (target < nums[n]) {
+                end -= 1;
+            }
+
+        }
+
+        start = 0;
+        end = nums.length - 1;
+
+        while(start <= end) {
+            int n = (start + end) / 2;
+            if (nums[n] == target) {
+                if (position[0] == -1) {
+                    position[0] = n;
+                }
+                if (n < position[0]) {
+                    position[0] = n;
+                }
+                end -= 1;
+            }
+            if(target > nums[n] ) {
+                start += 1;
+             } else if (target < nums[n]) {
+                end -= 1;
+            }
+        }
+
+        return position;
     }
 
     static int[] firstAndLastPositionLinearSearch(int[] nums, int target) {
@@ -31,6 +79,7 @@ public class BinarySearch {
         }
         return position;
     }
+
 
     static char nextGreatestLetter(char[] letters, char target) {
         String url = "https://leetcode.com/problems/find-smallest-letter-greater-than-target/";
