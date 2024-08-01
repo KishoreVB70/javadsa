7b6a9b705eae1dd1b3e7ee32ce94d322f6723a74
Kishore <kishore07kzr@gmail.com>
2024-08-01 16:15:23 +0530

    included the for loop version of simple bubble sort with a swap counter

--- src/Sorting/Bubble.java
+++ src/Sorting/Bubble.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Bubble {
     public static void main(String[] args) {
         int[] arr = {4,7,2,4,6,8,9,2};
-        int[] result = simpleBubbleSort(arr);
+        int[] result = SimpleBubbleSortWithForLoop(arr);
         System.out.println(Arrays.toString(result));
 
     }
@@ -28,8 +28,28 @@ public class Bubble {
             end--;
             turn++;
         }
-
         return arr;
+    }
 
+    static int[] SimpleBubbleSortWithForLoop(int[] nums) {
+        int end = nums.length - 1;
+        int swap = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (swap == 0) {
+                break;
+            }
+            for (int j = 0; j < end; j++) {
+                swap = 0;
+                if (nums[j] > nums[j+1]) {
+                    // Swap numbers
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j+1] = temp;
+                    swap++;
+                }
+            }
+            end--;
+        }
+        return nums;
     }
 }
