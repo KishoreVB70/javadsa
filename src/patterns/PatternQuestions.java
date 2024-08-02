2559c3335165234470a0ee6e08f5a156ae9cbd30
Kishore <kishore07kzr@gmail.com>
2024-08-02 14:15:54 +0530

    Solution for q3 of patterns

--- src/patterns/PatternQuestions.java
+++ src/patterns/PatternQuestions.java
@@ -2,8 +2,8 @@ package patterns;
 
 public class PatternQuestions {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            for (int j = 0; j <= i; j++) {
+        for(int i = 5; i >= 0; i--){
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -26,6 +26,40 @@ public class PatternQuestions {
             }
             System.out.println();
         }
+    }
+
+    static void q3(){
+        for(int i = 5; i >= 0; i--){
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
+    static void q4(){
+
+    }
+    static void q5(){
+
+    }
+    static void q6(){
+
+    }
+    static void q7(){
+
+    }
+    static void q8(){
+
+    }
+    static void q9(){
+
+    }
+    static void q10(){
+
+    }
+    static void q11(){
+
+    }
+
 }
