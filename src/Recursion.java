097d8e6b83cf4caba05c375267edce9bfaa79c79
Kishore <kishore07kzr@gmail.com>
2024-08-02 21:12:35 +0530

    Implemented recursion to solve fibo numbers

---
+++ src/Recursion.java
@@ -0,0 +1,32 @@
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(fibonacciNumber(0));
+    }
+
+
+    static int fibonacciNumber(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n <= 0) {
+            return n;
+        }
+        return (fibonacciNumber(n-1) + fibonacciNumber(n-2));
+    }
+    static void printNumbers(int n) {
+        if (n== 0) {
+            return;
+        }
+        printNumbers(n-1);
+        System.out.println(n);
+    }
+    static  void printHelloWorld(int n) {
+        if (n == 1) {
+            System.out.println("Hello world");
+            return;
+        }
+        System.out.println("Hello world");
+        printHelloWorld(n-1);
+    }
+}
