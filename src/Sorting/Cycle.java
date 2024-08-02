b323ef517f2ce690102de6cbf50f285b6defe7b7
Kishore <kishore07kzr@gmail.com>
2024-08-02 08:11:34 +0530

    Solved the problem of finding the missing number from 0 to N range

--- src/Sorting/Cycle.java
+++ src/Sorting/Cycle.java
@@ -4,9 +4,31 @@ import java.util.Arrays;
 
 public class Cycle {
     public static void main(String[] args) {
-        int[] nums = {6,2,4,5,3,1,};
-        simpleCycleSort(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = {1,2,0,4,3};
+        int missing = findMissingNumberIn0toN(nums);
+        System.out.println(missing);
+    }
+
+    static int findMissingNumberIn0toN(int[] nums) {
+        int i = 0;
+        int swaps = 0;
+        while (i < nums.length) {
+        if (swaps > nums.length - i) {
+            return i;
+        }
+            if (nums[i] == i) {
+                i++;
+                swaps = 0;
+            } else {
+                // If the supposedly last element is in the index
+                if (nums[i] == nums.length) {
+                    helperSwap(nums, i, ( nums[i] - 1));
+                }
+                helperSwap(nums, i, nums[i]);
+                swaps++;
+            }
+        }
+        return  i;
     }
 
     static void simpleCycleSort(int[] nums) {
