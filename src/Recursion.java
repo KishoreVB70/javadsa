1318a256ca89ebb501a75a28a1c16d90466f4580
Kishore <kishore07kzr@gmail.com>
2024-08-03 06:57:05 +0530

    Implemented binary search in recursion with ease

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,9 +1,30 @@
 public class Recursion {
     public static void main(String[] args) {
-        System.out.println(fibonacciNumber(0));
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,12,13};
+        System.out.println(binarySearch(nums, 0, nums.length - 1, 1));
     }
 
 
+    static int binarySearch(int[] nums, int start, int end, int target) {
+        // Base condition -> target is not in the array
+        if (start > end) {
+            return -1;
+        }
+
+        int n = ( start + end ) / 2;
+        int currentValue = nums[n];
+
+        if (currentValue == target) {
+            // Base condition -> target is found
+            return n;
+        }
+        else if (currentValue > target) {
+            return binarySearch(nums, start, n-1, target);
+        }
+        else  {
+            return binarySearch(nums, n+1, end, target);
+        }
+    }
     static int fibonacciNumber(int n) {
         if (n == 1) {
             return 1;
