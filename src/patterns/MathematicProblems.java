4244cfea30643a23c78cf80940c7ff61d670e24e
Kishore <kishore07kzr@gmail.com>
2024-08-04 08:59:33 +0530

    Implemented function to find one unique number in list of positive numbers using bitwise XOR operator

--- src/patterns/MathematicProblems.java
+++ src/patterns/MathematicProblems.java
@@ -2,9 +2,8 @@ package patterns;
 
 public class MathematicProblems {
     public static void main(String[] args) {
-        int[] nums = {-1,2,3,-4,-2,1,4};
-        System.out.println(findOneNonDuplicateNumberInNegativePositiveArray(nums));
-        System.out.println(isOdd(21));
+        int[] nums = {1,2,3,4,2,1,4};
+        System.out.println(findOneUniqueNumberInPositiveArray(nums));
     }
 
     static boolean isOdd(int n) {
@@ -17,6 +16,14 @@ public class MathematicProblems {
             sum += i;
         }
         return sum;
+    }
+
+    static  int findOneUniqueNumberInPositiveArray(int[] nums) {
+        int unique = 0;
+        for(int i: nums) {
+            unique = unique ^ i;
+        }
+        return unique;
     }
 }
 
