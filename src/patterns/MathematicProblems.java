5e2bd9328158223a0e1e1b51a7e7ef379e72600b
Kishore <kishore07kzr@gmail.com>
2024-08-04 08:55:09 +0530

    Implemented function to find one unique number in list of positive negative combination of numbers

--- src/patterns/MathematicProblems.java
+++ src/patterns/MathematicProblems.java
@@ -2,11 +2,21 @@ package patterns;
 
 public class MathematicProblems {
     public static void main(String[] args) {
+        int[] nums = {-1,2,3,-4,-2,1,4};
+        System.out.println(findOneNonDuplicateNumberInNegativePositiveArray(nums));
         System.out.println(isOdd(21));
     }
 
     static boolean isOdd(int n) {
         return ((n & 1) == 1);
+    }
+
+    static int findOneNonDuplicateNumberInNegativePositiveArray(int[] nums) {
+        int sum = 0;
+        for(int i: nums) {
+            sum += i;
+        }
+        return sum;
     }
 }
 
