bb8f3cb70f5f38fb8c214b7ef3daaa70e11e7929
Kishore <kishore07kzr@gmail.com>
2024-08-04 20:56:18 +0530

    Wrote program to get all the prime numbers till Nth number which is not optimized

--- src/patterns/MathematicProblems.java
+++ src/patterns/MathematicProblems.java
@@ -1,13 +1,35 @@
 package patterns;
 
+import java.util.Arrays;
+
 public class MathematicProblems {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,2,1,4};
-        System.out.println(unsetTheIthBit(9,4));
+        System.out.println(Arrays.toString(findAllThePrimeNumbersTillN(20)));
     }
 
     static boolean isOdd(int n) {
         return ((n & 1) == 1);
+    }
+
+    static int[] findAllThePrimeNumbersTillN(int n) {
+        int[] result = new int[20];
+        int k = 0;
+        for (int i = 2; i <= n; i++) {
+            boolean continuer = false;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    continuer = true;
+                    break;
+                }
+            }
+            if (continuer) {
+                continue;
+            }
+            result[k] = i;
+            k++;
+        }
+        return result;
     }
 
     static int findOneNonDuplicateNumberInNegativePositiveArray(int[] nums) {
