5c0901cc0ce01e83e0c8e51460016c29ec4e3d14
Kishore <kishore07kzr@gmail.com>
2024-08-06 13:36:29 +0530

    Streamlined quick sort

--- src/Sorting/Quick.java
+++ src/Sorting/Quick.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Quick {
     public static void main(String[] args) {
-        int[] nums  = {7,5,4,1,2,3,6};
+        int[] nums  = {5,1,2,3,4};
         quickSort(nums);
         System.out.println(Arrays.toString(nums));
     }
@@ -33,7 +33,7 @@ public class Quick {
                 --end;
             }
 
-            if (start == end) {
+            if (start >= end) {
                 ++start;
                 --end;
             } else {
@@ -45,6 +45,38 @@ public class Quick {
         // Quick sort for the right side
         helperQuickSort(nums, low, end);
         helperQuickSort(nums, start, high);
+    }
+    static void helperQuickSortKunalVersion(int[] nums, int low, int high) {
+        // Base condition
+        if (low >= high) {
+            return;
+        }
+
+        // Sorting the pivot
+        int start = low;
+        int end = high;
+        int pivot = nums[low + (end - low)/2];
+
+        while (start <= end) {
+            while (nums[start] < pivot) {
+                ++start;
+            }
+
+            while (nums[end] > pivot) {
+                --end;
+            }
+
+            if (start <= end) {
+                helperSwap(nums, start, end);
+                ++start;
+                --end;
+            }
+        }
+
+        // Quick sort for the right side
+        helperQuickSort(nums, low, end);
+        helperQuickSort(nums, start, high);
+
     }
     static void helperSwap(int[] nums, int indexA, int indexB) {
         int temp = nums[indexA];
