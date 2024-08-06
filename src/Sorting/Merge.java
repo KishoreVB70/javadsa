0785cdb63d6ba968e5f5bda0251f84d6c68e4a54
Kishore <kishore07kzr@gmail.com>
2024-08-06 10:14:28 +0530

    Successful implementation of inplace merge sort with only one additional array

--- src/Sorting/Merge.java
+++ src/Sorting/Merge.java
@@ -4,11 +4,70 @@ import java.util.Arrays;
 
 public class Merge {
     public static void main(String[] args) {
-        int[] nums = {2,6,5,4,1,3};
-        System.out.println(Arrays.toString(simpleMergeSort(nums)));
-
+        int[] nums = {2,5,4,1,3};
+        inPlaceMergeSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
+    static void inPlaceMergeSort(int[] nums) {
+        helperInPlaceMergeSort(nums, 0, nums.length - 1);
+    }
+
+    static void helperInPlaceMergeSort(int[] nums, int start, int end) {
+        if (start == end) {
+            return;
+        }
+
+        int m = (start + end ) / 2;
+
+        // Get result of two arrays
+        helperInPlaceMergeSort(nums, start, m);
+        helperInPlaceMergeSort(nums, m+1, end);
+
+        // Sort the arrays
+        int[] tempArray = new int[(end - start) + 1];
+
+        int i = start;
+        int j = m+1;
+        int k = 0;
+
+        while (i <= m && j <= end) {
+            if (nums[i] < nums[j]) {
+                tempArray[k] = nums[i];
+                i++;
+            } else {
+                tempArray[k] = nums[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= m) {
+            tempArray[k] = nums[i];
+            k++;
+            i++;
+        }
+
+        while (j <= end) {
+            tempArray[k] = nums[j];
+            k++;
+            j++;
+        }
+        k = 0;
+
+        // Copy over the array
+        while (k < tempArray.length ) {
+            nums[start] = tempArray[k];
+            start++;
+            k++;
+        }
+    }
+
+    static void helperSwap(int[] nums, int indexA, int indexB) {
+        int temp = nums[indexA];
+        nums[indexA] = nums[indexB];
+        nums[indexB] = temp;
+    }
     static int[] simpleMergeSort(int[] nums) {
 
         // Base termination condition
@@ -18,9 +77,9 @@ public class Merge {
 
 
         // Call half and half with recursion
-        int n = (nums.length - 1) / 2;
-        int[] one =  simpleMergeSort(Arrays.copyOfRange(nums, 0, n+1));
-        int[] two = simpleMergeSort(Arrays.copyOfRange(nums, n+1, nums.length));
+        int n = (nums.length) / 2;
+        int[] one =  simpleMergeSort(Arrays.copyOfRange(nums, 0, n));
+        int[] two = simpleMergeSort(Arrays.copyOfRange(nums, n, nums.length));
         int[] merged = new int[one.length + two.length];
 
         int i = 0;
@@ -38,18 +97,15 @@ public class Merge {
         }
 
         // One array is finished sorted, push the remaining array into the merged
-        if (i == one.length) {
-            while (j < two.length) {
-                merged[k] = two[j];
-                j++;
-                k++;
-            }
-        } else if (j == two.length) {
-            while (i < one.length) {
-                merged[k] = one[i];
-                i++;
-                k++;
-            }
+        while (j < two.length) {
+            merged[k] = two[j];
+            j++;
+            k++;
+        }
+        while (i < one.length) {
+            merged[k] = one[i];
+            i++;
+            k++;
         }
 
         return merged;
