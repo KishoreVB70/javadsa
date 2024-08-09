fde44ba3adb6a14b8d484a50ad4bd8305ec1b753
Kishore <kishore07kzr@gmail.com>
2024-08-09 20:41:48 +0530

    Added boilerplate code for custom linked list

---
+++ src/Structures/LinkedListProblems.java
@@ -0,0 +1,27 @@
+package Structures;
+
+public class LinkedListProblems {
+
+    class Node {
+
+        private int value;
+        private Node next;
+        private Node head;
+        private Node tail;
+
+        public  Node(int value) {
+            this.value = value;
+        }
+
+        public  Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
