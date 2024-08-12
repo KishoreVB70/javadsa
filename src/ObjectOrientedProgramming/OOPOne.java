f91fb9bf1e1c2017e2d328534b0e4e97970c3af0
Kishore <kishore07kzr@gmail.com>
2024-08-12 08:10:51 +0530

    Added constructor to the Student class

--- src/ObjectOrientedProgramming/OOPOne.java
+++ src/ObjectOrientedProgramming/OOPOne.java
@@ -8,22 +8,24 @@ public class OOPOne {
         Student kishore;
 
         // Initializing -> new keyword is used to dynamically allocate memory in runtime
-        kishore = new Student();
+        kishore = new Student("Kishore", 21);
 
         // kishore is the reference variable pointing to the memory address of that object
+
+        System.out.println(kishore.rollNum);
 
         // dot operator
         kishore.name = "kishore";
         kishore.rollNum = 7;
-        System.out.println(kishore);
     }
 
     static class Student {
         String name;
         int rollNum;
 
-
-
-
+        Student(String _name, int _rollNum) {
+            name = _name;
+            rollNum = _rollNum;
+        }
     }
 }
