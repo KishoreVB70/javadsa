a8fb16cd126434275aacc9b6c9699a47cb9845ee
Kishore <kishore07kzr@gmail.com>
2024-08-13 08:12:35 +0530

    Created functional generic Linked list

---
+++ src/Structures/CustomLinkedList.java
@@ -0,0 +1,81 @@
+package Structures;
+
+public class CustomLinkedList<T> {
+    public CustomLinkedList() {
+        Node temp = new Node();
+        head = temp;
+        tail = temp;
+    }
+
+    Node tail;
+    Node head;
+
+
+    public void addAtHead(T value) {
+        if (head.value == null) {
+            head.value = value;
+            return;
+        }
+
+        Node temp = new Node(value);
+        temp.next = head;
+        head = temp;
+    }
+
+    public void add(T value) {
+        // If fresh linked list
+        if (head.value == null) {
+            head.value = value;
+            return;
+        }
+
+        Node temp = new Node(value);
+        tail.next = temp;
+        tail = temp;
+    }
+
+    public T removeHead() {
+        // If head can't be removed
+        if (head == tail) {
+            return head.value;
+        }
+
+        T value = head.value;
+        head = head.next;
+        return value;
+    }
+
+    public T removeTail() {
+        // If tail can't be removed
+        if (head == tail) {
+            return head.value;
+        }
+
+        // 1) Find element before tail
+        Node current = head;
+        while (current.next != tail) {
+            current = current.next;
+        }
+
+        T value = tail.value;
+        // 2) Set it as the tail
+        tail = current;
+        tail.next = null;
+
+        return value;
+    }
+
+    private class Node {
+        T value;
+        Node next;
+
+        Node (T value) {
+            this.value = value;
+            this.next = null;
+        }
+        Node () {
+            this.value = null;
+            this.next = null;
+        }
+    }
+}
