2fb35eb7e42943902f185f3ea31ec0f57a2657bb
Kishore <kishore07kzr@gmail.com>
2024-08-19 11:38:42 +0530

    Implemented breath first search using an array list on binary search tree

--- src/Structures/BinQuestions.java
+++ src/Structures/BinQuestions.java
@@ -1,13 +1,55 @@
 package Structures;
 
+import java.util.ArrayList;
+
 public class BinQuestions {
     public static void main(String[] args) {
-        int[] bot = {3,8,6,7,-2,-8,4,9};
-        SegmentTre tree = new SegmentTre(bot);
+        BinSearchTree tree = new BinSearchTree(7);
+        for (int i = 0; i < 6; i++) {
+            tree.insert(i*2);
+        }
         tree.prettyDisplay();
-        tree.updateValue(0, 4);
-        System.out.println("-----------");
-        System.out.println();
-        tree.prettyDisplay();
+        bfsPrint(tree.root);
+    }
+
+    static class Node {
+        int value;
+        BinSearchTree.Node left;
+        BinSearchTree.Node right;
+        int height;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // 1 -> Breath first search
+    static void bfsPrint(BinSearchTree.Node root) {
+        if (root == null) {
+            return;
+        }
+        ArrayList<BinSearchTree.Node> nodes = new ArrayList<>();
+        nodes.add(root);
+        bfsPrint(nodes);
+    }
+    static void bfsPrint(ArrayList<BinSearchTree.Node> nodes) {
+        // Base condition
+        if (nodes.isEmpty()) {
+            return;
+        }
+
+        BinSearchTree.Node node = nodes.getFirst();
+
+        System.out.println(node.value);
+        if (node.left != null) {
+            nodes.add(node.left);
+        }
+        if (node.right != null) {
+            nodes.add(node.right);
+        }
+        nodes.removeFirst();
+        bfsPrint(nodes);
+
+
     }
 }
