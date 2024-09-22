c3a1b65132a051ff09fd98e682d5e15e58a2a66e
Kishore <kishore07kzr@gmail.com>
2024-09-22 21:24:25 +0530

    Solvd prodcut except self array problem

--- src/Arrays/ArrayProblems.java
+++ src/Arrays/ArrayProblems.java
@@ -8,6 +8,25 @@ public class ArrayProblems {
 
 
     // Striver Medium problems
+
+    // 14) Product except self
+    // Medium https://leetcode.com/problems/product-of-array-except-self
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        return ans;
+    }
     // 13) H Index
     // Medium https://leetcode.com/problems/h-index/
     public int hIndex(int[] citations) {
