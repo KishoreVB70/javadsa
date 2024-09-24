691aed0c7880f799c02d628a3e89814504852a6e
Kishore <kishore07kzr@gmail.com>
2024-09-24 11:23:24 +0530

    Solved the four sum problem and the longest sub array with zero sums geek problem

--- src/Arrays/ArrayProblems.java
+++ src/Arrays/ArrayProblems.java
@@ -4,11 +4,83 @@ import java.util.*;
 
 public class ArrayProblems {
     public static void main(String[] args) {
-        System.out.println(majorityElement2(new int[]{1,2}));
+        List<List<Integer>> res = fourSum(new int[]{1000000000,1000000000,1000000000,1000000000}, -294967296);
+        System.out.println(res.toString());
     }
 
 
+
+
     // Striver Medium problems
+
+    // Largest sub array with 0 sum
+    // Geek problem
+    public int largestSubArrayZeroSum(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int longest = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(map.containsKey(sum)) {
+                longest = Integer.max(longest, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return longest;
+    }
+
+    // 4Sum
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i< nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            for(int j = i+1; j < nums.length; j++) {
+                if(j > i+1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+                int k = j+1;
+                int l = nums.length -1;
+                while(k < l) {
+                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target) {
+                        List<Integer> lt = new ArrayList<>();
+                        lt.add(nums[i]);
+                        lt.add(nums[j]);
+                        lt.add(nums[k]);
+                        lt.add(nums[l]);
+                        result.add(lt);
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l+1]) {
+                            l--;
+                        }
+                    }
+
+                    if(sum > target) {
+                        l--;
+                        while(k < l && nums[l] == nums[l+1]) {
+                            l--;
+                        }
+                    }
+
+                    if(sum < target) {
+                        k++;
+                        while(k < l && nums[k] == nums[k-1]) {
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
     public static List<Integer> majorityElement2(int[] nums) {
         List<Integer> s = new ArrayList<>();
