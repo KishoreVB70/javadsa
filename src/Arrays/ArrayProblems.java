8fe5becb9b5c8120ef4d45eafa42a31ab9e957fe
Kishore <kishore07kzr@gmail.com>
2024-09-25 22:08:50 +0530

    Solved leet easy problem of removing element

--- src/Arrays/ArrayProblems.java
+++ src/Arrays/ArrayProblems.java
@@ -12,6 +12,16 @@ public class ArrayProblems {
 
 
     // Striver Medium problems
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
 
     // Hard ->
     static int count = 0;
