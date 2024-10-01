a9c200b4af7aa1a2c4c4757ab540c22ef7bcc2c7
Kishore <kishore07kzr@gmail.com>
2024-10-01 10:43:36 +0530

    Solved longest odd number problem in string - Easy

--- src/StringProblems.java
+++ src/StringProblems.java
@@ -1,7 +1,24 @@
 public class StringProblems {
     public static void main(String[] args) {
-        String s = "abba";
-        System.out.println(findPalindrome(s));
+        String s = "52";
+        System.out.println(largestOddNumber(s));
+    }
+
+    // Striver easy problems
+    // 3) Easy Largest odd number
+    public static String largestOddNumber(String num) {
+        StringBuilder ans = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        for(char c: num.toCharArray()) {
+            int v = (int)(c - '0');
+            temp.append(c);
+            // If it is an odd number
+            if( v % 2 != 0) {
+                ans = new StringBuilder(temp);
+            }
+        }
+
+        return ans.toString();
     }
 
     static boolean findPalindrome(String s) {
