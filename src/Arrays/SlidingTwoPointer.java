13fe6cd845c3b8fcbf5f0e98c675f9bf4481cda9
Kishore <kishore07kzr@gmail.com>
2024-10-08 20:52:29 +0530

    Solved longest consecutive one's III using sliding window in O(2N) time and O(N) space complexity

--- src/Arrays/SlidingTwoPointer.java
+++ src/Arrays/SlidingTwoPointer.java
@@ -1,7 +1,36 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SlidingTwoPointer {
+    public static void main(String[] args) {
+    }
     // Striver medium problems
+
+    // Maximum consecutive ones III
+    public int longestOnes(int[] nums, int k) {
+        int zero = 0;
+        int max = 0;
+        int start = 0;
+        for(int i =0; i< nums.length; i++) {
+            if(nums[i] == 0) {
+                zero++;
+                if(zero > k) {
+                    // Move on till you skip the first zero
+                    while(nums[start] != 0) start++;
+                    // Now you have reached the first zero
+                    start++;
+                    zero--;
+                }
+            }
+            max = Math.max(max, (i +1) - start);
+        }
+
+        return max;
+
+    }
 
     // Removing duplicates II
     // Medium https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
