5f022c0fa7c3ea53063c4daf6b798c9009b634f2
Kishore <kishore07kzr@gmail.com>
2024-10-12 20:47:21 +0530

    Solved gas station problem

--- src/Arrays/SlidingTwoPointer.java
+++ src/Arrays/SlidingTwoPointer.java
@@ -12,6 +12,23 @@ public class SlidingTwoPointer {
 //        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
     }
 
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sGas = 0, sCost = 0, res = 0, total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sGas += gas[i];
+            sCost += cost[i];
+        }
+        if (sGas < sCost) return -1;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            if (total < 0) {
+                total = 0;
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+
     public int maxProfit(int[] prices) {
         int max = 0;
         int start = prices[0];
