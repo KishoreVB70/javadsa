3da76bc33178c4cde9b384ca2c3eaf96a18aaede
Kishore <kishore07kzr@gmail.com>
2024-10-13 17:46:12 +0530

    Solved a similar easy problem of assigning cookies

--- src/ArraysProblems/GreedyAlgos.java
+++ src/ArraysProblems/GreedyAlgos.java
@@ -7,7 +7,28 @@ public class GreedyAlgos {
 //        System.out.println(matchPlayersAndTrainers());
     }
 
+    // 1 B) Assign Cookies
+    // Easy https://leetcode.com/problems/assign-cookies/
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
 
+        int satisfied = 0;
+        int i = 0;
+        int j = 0;
+
+        while(i < g.length && j < s.length) {
+            // Sastisfaction
+            if(s[j] >= g[i]) {
+                satisfied++;
+                i++;
+            }
+            // Not satisfied -> only j moves ahead
+            j++;
+        }
+
+        return satisfied;
+    }
     // 1) Match players and trainers
     // Medium https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
     public static int matchPlayersAndTrainers(int[] players, int[] trainers) {
